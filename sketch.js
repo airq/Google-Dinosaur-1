e430f5214af3ed02d1a2b45a40a5bc573caa28fd
masakrow <michalc00@gmail.com>
2018-07-08 12:21:34 +0200

    Added collision function. Otymalization of creation enemy.

--- sketch.js
+++ sketch.js
@@ -9,40 +9,62 @@ let accPlayer=0;
 let enemy;
 let Arrenemy = [];
 
+let col;
+
 function setup() {
   createCanvas(700,201);
   enemy = new Cactus();
   Arrenemy.push(enemy);
+
 }
 
 function draw() {
   background(200);
 
-  let distance = floor(random(50))
-  if(Arrenemy[Arrenemy.length-1].x < 500+distance ){
-      enemy = new Cactus();
-      enemy.width+=floor(random(10));
-      enemy.height+=floor(random(10));
-      enemy.y= height- enemy.height;
-      Arrenemy.push(enemy);
+  if(!col){
+  let distance = floor(random(60))
+    if(Arrenemy[Arrenemy.length-1].x < 500+distance ){
+        enemy = new Cactus();
+        enemy.width+=floor(random(10));
+        enemy.height+=floor(random(10));
+        enemy.y= height- enemy.height-1;
+        Arrenemy.push(enemy);
+      }
+
+    accPlayer=accPlayer+0.2;
+    if (accPlayer>5){
+      accPlayer=5;
+    }
+    playerY=playerY+accPlayer;
+    if((playerY+playerHeight)>(height-1)){
+      playerY=150;
+      accPlayer=0;
     }
 
-  accPlayer=accPlayer+0.2;
-  if (accPlayer>5){
-    accPlayer=5;
+    for(let i=0; i<Arrenemy.length; i++){
+      if(Arrenemy[i].x < 0-Arrenemy[i].width){
+        Arrenemy.reverse();
+        Arrenemy.pop();
+        Arrenemy.reverse();
+      }
+      Arrenemy[i].show();
+    }
+    fill(255);
+    rect(playerX, playerY, playerWidth, playerHeight)
+    collision();
   }
-  playerY=playerY+accPlayer;
-  if((playerY+playerHeight)>(height-1)){
-    playerY=150;
-    accPlayer=0;
-  }
+
+
+}
+
+function collision(){
 
   for(let i=0; i<Arrenemy.length; i++){
-    Arrenemy[i].show();
+    if ((playerX<Arrenemy[i].x) && ((playerX+playerWidth)>(Arrenemy[i].x)) && (playerY<(Arrenemy[i].y)) && ((playerY+playerHeight)>(Arrenemy[i].y ))){
+      console.log("Collision");
+      col=1;
+    }
   }
-  fill(255);
-  rect(playerX, playerY, playerWidth, playerHeight)
-
 }
 
 function keyPressed(){
